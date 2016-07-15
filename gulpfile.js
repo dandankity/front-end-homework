8cc046d6df96319c304df3d4346015bba4e9f9eb
Dan Chen <dchen@thoughtworks.com>
2016-07-15 22:17:17 +0800

    remove app folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gls = require('gulp-live-server');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
-	return gulp.src('app/scss/**/*.scss')
+	return gulp.src('./scss/**/*.scss')
 	.pipe(sass())
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest('./css'))
 	.pipe(browserSync.reload({
       stream: true
     }));
@@ -15,19 +15,19 @@ gulp.task('sass', function(){
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: './'
     },
   })
 });
 
 gulp.task('start', ['browserSync','sass'], function() {
   //2. serve at custom port
-  var server = gls.static('app/', 3000);
+  var server = gls.static('./', 3000);
   // var server = gls('./', true, 3030);
   server.start();
 
   //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch(['app/scss/*.scss', 'app/index.html'], ['sass'], function (file) {
+  gulp.watch(['./scss/*.scss', './index.html'], ['sass'], function (file) {
     server.notify.apply(server, [file]);
   });
 });
